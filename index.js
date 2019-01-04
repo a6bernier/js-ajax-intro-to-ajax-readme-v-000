2c4a15d5ffca955bdae0a1962c9fe1c6ea4994ba
Andrew Bernier <a6bernier@gmail.com>
2019-01-04 19:10:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ function showRepositories() {
   console.log(this.responseText);
 }
 
-
-
-
 function getRepositories() {
   const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
